a1ee5a46dba482e5c357028ebcfc9b0b8517ff9c
Jacob Nuno <jacobcnda@gmail.com>
2019-07-26 12:14:58 -0500

    flag variable definition missing

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ app.get('/webhook/', function(req, res) {
 });
 
 app.post('/webhook/', function(req, res) {
+	let flag = false;
 	let messaging_events = req.body.entry[0].messaging;
 	let zip_code = 0;
 	for(let i = 0; i < messaging_events.length; i++) {
